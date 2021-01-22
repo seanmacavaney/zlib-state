4d83725fde0ef306e7080569ed000640e598f75e
Sean MacAvaney <sean.macavaney@gmail.com>
2021-01-22 15:54:48 +0000

    added bdist deployment to actions and bumped version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setuptools.setup(
     name="zlib-state",
-    version="0.1.0",
+    version="0.1.1",
     author="Sean MacAvaney",
     author_email="sean.macavaney@gmail.com",
     description="Low-level interface to the zlib library that enables capturing the decoding state",
