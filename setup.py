cca59a9d54e299b995e1c6a21ddb4ca5592c2cd0
Sean MacAvaney <sean.macavaney@gmail.com>
2021-01-22 22:00:11 +0000

    kitchen sink

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ if windows:
     ZLIB_HOME = os.environ.get("ZLIB_HOME", "C:/Program Files/zlib")
     include_dirs.append(os.path.join(ZLIB_HOME, "include"))
     libs.append('zlib')
-    libs.append('zlibstatic')
+    libs.append(ZLIB_HOME + "\\lib\\zlibstatic")
+    libs.append("/IMPLIB:" + ZLIB_HOME + "\\lib\\zlibstatic")
+    libs.append("/IMPLIB:" + ZLIB_HOME + "\\lib\\zlibstatic.lib")
     lib_dirs.append(ZLIB_HOME + "\\lib")
 else:
     libs.append('z')
